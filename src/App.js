3285844c8ee34f4ec165cffa70b13b60bcae54a4
Pedro <pedroshalommrn@hotmail.com>
2022-08-18 17:13:31 -0300

    add req 7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Missions from './components/Missions';
 import PlanetCard from './components/PlanetCard';
 import SolarSystem from './components/SolarSystem';
 
@@ -11,6 +12,7 @@ class App extends React.Component {
         <Header />
         <SolarSystem />
         <PlanetCard />
+        <Missions />
       </div>
     );
   }
