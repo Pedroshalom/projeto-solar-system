e0c8bf513b1bf9438e5bd7ce5344d5466ff60fe0
Pedro <pedroshalommrn@hotmail.com>
2022-08-18 21:34:38 -0300

    add req 10

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import MissionCard from './MissionCard';
 import Title from './Title';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
+    console.log(missions);
     return (
       <div>
         <div data-testid="missions" />
         <Title headline="Missões" />
+        {missions.map((element) => (
+          <MissionCard
+            key={ element.name }
+            name={ element.name }
+            year={ element.year }
+            country={ element.country }
+            destination={ element.destination }
+          />
+        ))}
       </div>
     );
   }
 }
-
 export default Missions;
